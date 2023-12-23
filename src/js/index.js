b03f7405ca3eccdadd4154aee3a439ffdb928723
Fakhry224 <112813649+Fakhry224@users.noreply.github.com>
2023-12-23 17:17:57 +0700

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //* Responsive Function
 window.addEventListener("DOMContentLoaded", (event) => {
   var windowWidth = window.innerWidth;
+  var rowElement = document.querySelector(".half");
 
   var responsiveImg1 = document.getElementById("responsiveImg1");
   var responsiveImg2 = document.getElementById("responsiveImg2");
@@ -16,6 +17,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
     responsiveImg3.src = "assets/images/demo3.png";
   }
 
+  if (windowWidth <= 768) {
+    if (rowElement) {
+      rowElement.classList.remove("row50");
+      rowElement.classList.add("row100");
+    }
+  }
+
   window.addEventListener("resize", function () {
     var windowWidth = window.innerWidth;
 
@@ -23,8 +31,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
       responsiveImg1.src = "assets/images/demores1.png";
       responsiveImg2.src = "assets/images/demores2.png";
       responsiveImg3.src = "assets/images/demores3.png";
-    } else if (windowWidth >= 768 && windowWidth <= 992) {
-      var rowElement = this.document.querySelector(".half");
+    } else if (windowWidth <= 768) {
       if (rowElement) {
         rowElement.classList.remove("row50");
         rowElement.classList.add("row100");
@@ -34,7 +41,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
       responsiveImg2.src = "assets/images/demo2.png";
       responsiveImg3.src = "assets/images/demo3.png";
 
-      var rowElement = document.querySelector(".half");
       if (rowElement) {
         rowElement.classList.remove("row100");
         rowElement.classList.add("row50");
@@ -63,6 +69,12 @@ function pagesController(destination) {
       break;
     case "service":
       window.location.href = "./src/pages/service-page.html";
+      break;
+    case "sell":
+      window.location.href = "./sell-page.html";
+      break;
+    case "buy":
+      window.location.href = "./buy-page.html";
       break;
     default:
       console.log("Destination tidak valid");
